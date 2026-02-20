69159a7d3be98c12bc544bc7367342f7105582f2
Soulaiman Elkhalidi <elkhalidisolayman@gmail.com>
2026-02-20 20:26:02 +0100

    Initial commit

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use("/images", express.static(path.join(__dirname, "images")));
 dotenv.config();
 
 const app = express();
